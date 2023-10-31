addc2f42d329ed4f8c9c043d7ee3fdbb0717a083
Anatolii <mavtodrug@mail.ru>
2023-10-31 16:50:23 +0500

    work#2 add db

--- crm_client/views.py
+++ crm_client/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView
@@ -15,39 +17,28 @@ class clientListView(SingleTableView):
     table_class = Crm_client_Table
     template_name = 'crm_client/base.html'
     extra_context = {'form': form}
+    def post(self,request):
+        if request.method == 'POST':
+            f = AddRecordClient(request.POST)
+            if f.is_valid():
+                f.save()
+                message = 'Запись добавлена успешно'
+                mess(request,message)
+        else:
+            message = 'Ошибка, запись не добавлена!'
+            mess(request,message)
+
+        return HttpResponseRedirect('/client/')
+def mess(request,message):
+    messages.success(request, message)
+
+def save_client():
+    print('delete')
+
+# class CreateCRMClientView(FormView):
+#     template_name = 'crm_client/base.html'
+#     form_class = AddRecordClient
+#     success_url = reverse_lazy('/')
 
 
 
-
-def dry(request):
-
-    print('srabotalo')
-    f=AddRecordClient(request.POST)
-    f.save()
-
-
-
-
-class CreateCRMClientView(FormView):
-    template_name = 'crm_client/base.html'
-    form_class = AddRecordClient
-    #success_url = reverse_lazy('client')
-
-
-    def save_client(self,form):
-        client = Crm_client(name=form.cleaned_data['name'],
-                            surname=form.cleaned_data['surname'],
-                            patronymic=form.cleaned_data['patronymic'],
-                            car=form.cleaned_data['car'],
-                            telephone=form.cleaned_data['telephone'],
-                            vin=form.cleaned_data['vin'])
-        client.save()
-        return super().form_valid(form)
-# def View_forms(request):
-#     form = AddRecordClient()
-#     return render(request, "crm_client/base.html")
-
-
-# def view_table(request):
-#     table = Crm_client_Table(Crm_client.objects.all())
-#     return render(request, 'crm_client/base.html', {'table': table})
