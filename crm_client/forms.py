0464873289b09d72d7afc827cd3d34f7baca2e0d
Anatolii <mavtodrug@mail.ru>
2023-11-13 16:58:56 +0500

    commit add table | create  update,delete,message error

--- crm_client/forms.py
+++ crm_client/forms.py
@@ -19,6 +19,7 @@ from crm_client.views import *
 
 class AddRecordClient(forms.ModelForm):
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.fields['name']= forms.CharField(required=False, label=False, widget=forms.TextInput(attrs={"class":"myfield",'placeholder': 'Имя'}))
         self.fields['surname']=forms.CharField(label=False,required=False, widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}))
@@ -48,6 +49,9 @@ class AddRecordClient(forms.ModelForm):
         #     'vin': forms.TextInput(attrs={'placeholder': 'VIN-номер'}),
         # }
 
+
+
+
     def clean(self):
         name = self.cleaned_data['name']
         surname = self.cleaned_data['surname']
@@ -71,3 +75,29 @@ class AddRecordClient(forms.ModelForm):
         if not vin:
             raise forms.ValidationError('Поле "VIN" не должно быть пустым')
         return vin
+
+class UpdateClient(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.fields['name']= forms.CharField(required=False, label=False, widget=forms.TextInput(attrs={"class":"myfield",'placeholder': 'Имя'}))
+        self.fields['surname']=forms.CharField(label=False,required=False, widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}))
+        self.fields['patronymic'] = forms.CharField(label=False,required=False, widget=forms.TextInput(attrs={'placeholder': 'Отчество'}))
+        self.fields['car'] = forms.ModelChoiceField(empty_label='Авто',label=False, required=False, queryset=BrandAuto.objects.all())
+        self.fields['telephone'] = forms.CharField(label=False, required=False, widget=forms.TextInput(attrs={'placeholder': 'Телефон'}))
+        self.fields['vin'] =forms.CharField(label=False, required=False, widget=forms.TextInput(attrs={'placeholder': 'VIN-номер'}))
+
+    class Meta:
+        model = Crm_client
+        fields = [ 'name', 'surname', 'patronymic', 'car', 'telephone', 'vin']
+
+
+    def clean(self):
+        name = self.cleaned_data['name']
+        surname = self.cleaned_data['surname']
+        patronymic = self.cleaned_data['patronymic']
+        car = self.cleaned_data['car']
+        telephone = self.cleaned_data['telephone']
+        vin = self.cleaned_data['vin']
+        if not name and not surname and not patronymic and not car and not telephone and not vin:
+            raise forms.ValidationError('Вы не ввели ни одного поля для редактирования')
