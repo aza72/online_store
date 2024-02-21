3e1a9de006958b30f37bb3b1fa86ffb62f5a1a0e
aza72 <mavtodru@mail.ru>
2024-02-21 21:06:05 +0500

    создание шапки home

--- crm_client/templatetags/common_tags.py
+++ crm_client/templatetags/common_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 def show_main_menu():
     menu_items = ['Клиент','Бонусная система','Заказы','Статистика','Клиентская база',]
     return {"menu_items": menu_items,}
+
+@register.inclusion_tag('tmptag/header.html')
+def show_header_menu():
+    header_menu_items = ['Войти','Регистрация',]
+    return {"header_menu_items": header_menu_items,}
